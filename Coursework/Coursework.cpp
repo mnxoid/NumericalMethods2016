f89a7971c1680a9527ab2a8d2f9ba7bf00ecc04a
Dudyak Michael <sergolwow@gmail.com>
2016-12-03 21:30:18 +0200

    Libs added

--- Coursework/Coursework.cpp
+++ Coursework/Coursework.cpp
@@ -1,11 +1,27 @@
 // Coursework.cpp : Defines the entry point for the console application.
 //
 
+// Vanilla includes:
 #include "stdafx.h"
+#include <iostream>
 
+// Eigen or boost includes:
+#include <Eigen/Core>
+
+// Namespaces
+using namespace std;
+using namespace Eigen;
 
 int main()
 {
+	cout << "Hello, world!" << endl;
+	// TIP: there is a Dynamic predefined value for dynamic size
+	Matrix<int, 3, 3> M;
+	M << 1, 2, 3,
+		4, 5, 6,
+		7, 8, 9;
+	//VectorXd v(5);
+	system("pause");
     return 0;
 }
 
