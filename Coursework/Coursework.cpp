e0bc58a6b737f3ef8e2917155e451d503dc2f246
Dudyak Michael <sergolwow@gmail.com>
2016-12-10 22:35:44 +0200

    App semi-finished. Still needs some polish but it's okay.

--- Coursework/Coursework.cpp
+++ Coursework/Coursework.cpp
@@ -4,31 +4,41 @@
 // Vanilla includes:
 #include "stdafx.h"
 #include <iostream>
+#include <chrono>
 
 // In-project includes
 #include "util.h"
+#include "Newton.h"
+#include "NewtonModified.h"
 
 // Namespaces
 using namespace std;
+using namespace std::chrono;
+
 
 // Main function
 int main()
 {
-	cout << "Hello, world!" << endl;
-	// TIP: there is a Dynamic predefined value for dynamic size
-	Matrix<int, 3, 3> M;
-	M << 1, 2, 3,
-		4, 5, 6,
-		7, 8, 9;
-	cout << M << endl;
-	Matrix<dfun, 3, 1> F;
-	F << [](Vector3d x) {return x[0] + 4; },
-		[](Vector3d x) {return x[1] * 4; },
-		[](Vector3d x) {return x[2] - 4; };
-	Vector3d v;
-	v << 3.4, 5.6, 1.3;
-	cout << apply(F,v) << endl;
-	//VectorXd v(5);
+	Matrix<dfun2, 2, 1> F; //!< The NES
+	F << [](const Vector2d &x) {return x[0] + x[1] - 3; },
+		[](const Vector2d &x) {return x[0] * x[0] + x[1] * x[1] - 9; };
+	Matrix<dfun2, 2, 2> W; //!< The Jacobian
+	W << [](const Vector2d &x) {return 1; }, [](const Vector2d &x) {return 1; },
+		[](const Vector2d &x) {return 2 * x[0]; }, [](const Vector2d &x) {return 2 * x[1]; };
+	Newton N;
+	NewtonModified N1;
+	for(Newton* n : {&N, static_cast<Newton*>(&N1)})
+	{
+		n->F = F;
+		n->W = W;
+		n->eps = 0.000000001;
+		n->debug = false;
+		high_resolution_clock::time_point t1 = high_resolution_clock::now();
+		n->main();
+		high_resolution_clock::time_point t2 = high_resolution_clock::now();
+		auto duration = duration_cast<microseconds>(t2 - t1).count();
+		cout << "Time elapsed: " << duration << "mcs" << endl;
+	}
 	system("pause");
     return 0;
 }
